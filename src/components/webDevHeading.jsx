a8853f6d2f4260fa273158a06a4ed95580f5fc8f
Dax <darronmgordon@gmail.com>
2022-07-22 04:40:24 +0200

    webDev Heading Fix

--- src/components/webDevHeading.jsx
+++ src/components/webDevHeading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function WebDevHeading(){
 return(<div className="devHeader">
-<img src="webDevHeading.png" alt="WebDevelopment"/>
+<img src="webDevHeading2.jpg" alt="WebDevelopment"/>
 </div>);
 }
 export default WebDevHeading;
