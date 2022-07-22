5ce675cfe5ec7ef7ba4f378e411a3fdc232d7f47
Dax <darronmgordon@gmail.com>
2022-07-22 04:50:22 +0200

    headiing background img

--- src/components/webDevHeading.jsx
+++ src/components/webDevHeading.jsx
@@ -3,7 +3,6 @@ import React from "react";
 
 function WebDevHeading(){
 return(<div className="devHeader">
-<img src="webDevHeading2.jpg" alt="WebDevelopment"/>
 </div>);
 }
 export default WebDevHeading;
